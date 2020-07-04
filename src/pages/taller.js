763e1e6e4e020df6d032f49c01a278fb9cca4b8f
camunoz2 <camunoz2@gmail.com>
2020-07-04 00:15:58 -0400

    descripcion proximo taller

--- src/pages/taller.js
+++ src/pages/taller.js
@@ -3,8 +3,50 @@ import Layout from "../components/layout"
 
 const Taller = () => {
   return (
-    <Layout headerTitle="Proxiamente">
-      <div>...</div>
+    <Layout headerTitle="Taller de preparacion al parto en casa">
+      <div className="text-md md:text-xl w-3/4 mx-auto font-light">
+        <p className="mb-4">
+          El parto en casa, hoy en Chile, es una realidad, estoy muy contenta,
+          agradecida y emocionada, de la gran acogida que tiene el trabajo que
+          estamos realizando, muchas matronas y doulas en nuestro país en el
+          acompañamiento de familias a parir en casa.
+        </p>
+
+        <p className="mb-4">
+          Parir en casa es una opción viable, sana, segura y legal. Las matronas
+          tenemos la preparación y las herramientas profesionales para
+          acompañarte en esta decisión.
+        </p>
+
+        <p className="mb-4">
+          Para decidir parir en casa, es muy necesario estar informados de todos
+          los requerimientos, de la seguridad, los protocolos, la legalidad, el
+          rol del o l@s acompañantes en el proceso, la fisiología del proceso
+          entre otras cosas, como que es el plan B? Distancia a un centro
+          asistencial, atención inmediata al recién nacido, exámenes, vacunas, y
+          varias otras cositas. Es por lo anterior que he creado este taller
+          que, para mí, es de carácter fundamental para acompañarte en tu parto
+          en casa.
+        </p>
+
+        <ul>
+          <li>
+            - Legalidad del parto en casa, documentos e inscripción del recien
+            nacido.
+          </li>
+          <li>
+            - Lugar en que ocurrirá el parto; ambiente físico, emocional,
+            personas presentes, la importancia de la participación de los hijos.
+          </li>
+          <li>- Contexto de pandemia.</li>
+          <li>- Emergencias en el parto en casa. Plan B.</li>
+          <li>- Atención inicial del recién nacido.</li>
+          <li>- Quienes pueden parir en casa?</li>
+          <li>- Que se necesita para parir en casa?</li>
+          <li>- Beneficios de parir en casa.</li>
+          <li>- en que consiste el servicio que prestamos.</li>
+        </ul>
+      </div>
     </Layout>
   )
 }
