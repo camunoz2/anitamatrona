862ca3cd52b89fa4cf88c8e751f17c6a1b4c0307
camunoz2 <camunoz2@gmail.com>
2020-07-06 16:15:12 -0400

    agregue purgecss para probar estilos no en head

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,19 @@ module.exports = {
     baseUrl: "https://anitamatrona.cl", // No trailing slash allowed!
     image: "/images/site_image.png",
   },
-  plugins: [`gatsby-plugin-postcss`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      },
+    },
+  ],
 }
