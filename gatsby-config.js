098c95126a407b1f6371002630e7f578a6d41353
camunoz2 <camunoz2@gmail.com>
2020-07-05 21:56:15 -0400

    agregue seo a layout con helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`],
+  siteMetadata: {
+    title: "Anita Matrona - Atención respetuosa de la mujer sus procesos",
+    description:
+      "En esta página encontraras material interesante con respecto a mi trabajo, la salud de la mujer, sus procesos y cambios a lo largo de su ciclo vital.",
+    baseUrl: "https://anitamatrona.cl", // No trailing slash allowed!
+    image: "/images/site_image.png",
+  },
+  plugins: [`gatsby-plugin-postcss`, `gatsby-plugin-react-helmet`],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,46 @@
 import React from "react"
+
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+
 import Header from "./header"
 import Navbar from "./navbar"
 import GoUp from "./go-up"
 import Footer from "./footer"
 
 const Layout = ({ children, headerTitle, showImage }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          baseUrl
+          title
+          description
+          image
+        }
+      }
+    }
+  `)
+
+  const metadata = data.site.siteMetadata
+
   return (
     <>
+      <Helmet>
+        <title>{metadata.title}</title>
+        <link rel="canonical" href={metadata.baseUrl} />
+        <meta name="description" content={metadata.description} />
+        {metadata.image && <meta name="image" content={metadata.image} />}
+
+        <meta property="og:url" content={metadata.url} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        {metadata.image && (
+          <meta property="og:image" content={metadata.image} />
+        )}
+      </Helmet>
+
       <div className="container mx-auto px-4">
         <Navbar />
         <Header title={headerTitle} showImage={showImage} />
