208f67aa6928f8d775824921775b8d991602a7fe
indicestudio <indicestudio@gmail.com>
2020-10-28 00:31:50 -0300

    cambie token IG

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,7 +5,7 @@ exports.sourceNodes = async ({
 }) => {
   // get data from Instagram API at build time
   const result = await fetch(
-    `https://graph.instagram.com/me/media?fields=caption,media_type,media_url,timestamp,thumbnail_url,permalink&access_token=IGQVJXQUhCcnJQZAGNxRlI5RDlVcnhxdmFiZAUJhcTlSS0pQWWxQWm1nNXNuTFgxZAkRISGRtRTI2ME9ZAVFhESWhkemc1ZAmpfbl9Eb1hpSm56dXRFT00wMlhGZA0pUVmdWSzI2ZA1otTnlhdm5nc1JsdWkxXwZDZD`
+    `https://graph.instagram.com/me/media?fields=caption,media_type,media_url,timestamp,thumbnail_url,permalink&access_token=IGQVJWSjhrcWxQWVVvbHA5anFseE9yY3JWUkJtbDh5OGVTUDhSOU9ua3REWFpwTDltcFAwTmJOSnhTTVhnOUVnZAEVhRGIzZAXZARX2ZAOUkU4TkI0dUdvemEySW1MNTd0M0NHU1lvOTV3`
   )
   const resultData = await result.json()
 
