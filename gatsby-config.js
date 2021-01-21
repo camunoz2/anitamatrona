5e3c3dc355869b31892cd8f514952ebec17c1262
Cristian <camunoz2@gmail.com>
2021-01-21 20:58:03 -0300

    fix GA4

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,20 +53,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-PSHB3FFRMY",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        
-      },
-    },
-    {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         printRejected: true, // Print removed selectors and processed file names
